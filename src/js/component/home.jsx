fbcbee18c4d43daeeebf07c6246c927451e44887
Patricio Espinoza <p.alfonsoespinoza@gmail.com>
2022-08-31 12:15:41 -0400

    todolist with react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,61 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useRef, useState } from "react";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+function Todolist() {
+  let nombreRef = useRef(null);
+  const [task, setTask] = useState ([]);
 
-export default Home;
+  const addTask = (e) => {
+    if (e.keyCode === 13 && nombreRef.value !== "") {
+      setTask(task.concat(nombreRef.value));
+      nombreRef.value = "";
+    }
+  };
+
+  const deleteTask = (index) =>{
+    task.splice(index,1);
+    setTask([...task]);
+  };
+
+  return (
+    <div className="container">
+      <div className="card mt-4">
+        <div className="card-body">
+          <h1 className="card-title text-center">To do list <i className="fas fa-tasks"></i></h1> 
+          <ul className="list-group list-group-flush">
+            <div className="input-group mb-2 list-group list-group-flush">
+              <input
+                onKeyUp={addTask}
+                ref={(r) => (nombreRef = r)}
+                type="text"
+                id="input"
+                class="list-group-item"
+                placeholder="What needs to be done?"
+              />
+            </div>
+
+            {!!task.length > 0 &&
+              task.map((valor, index) => {
+                return (
+                  <li class="list-group-item" key={index}>
+                    {valor}{" "}
+                    <i
+                      className="fas fa-trash float-right"
+                      id="eliminar"
+                      onClick={() => deleteTask(index)}
+                    ></i>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+        <div className="card-footer text-muted">
+          Things to do {task.length}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Todolist;
